d4b77240cae1eb95bc32bb7348e7c4c96a9a8d6c
AmirHosein Mardani <139256057+this-ILECY@users.noreply.github.com>
2024-11-22 12:53:40 +0330

    contact + phone!

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -4,8 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'app-contact',
     templateUrl: './contact.component.html',
-    styleUrls: ['./contact.component.scss'],
-    imports: [NgFor]
+    styleUrls: ['./contact.component.scss']
 })
 export class ConactComponent implements OnInit {
     constructor() { }
@@ -14,6 +13,17 @@ export class ConactComponent implements OnInit {
         { sender: "Amirhosein Mardani", content: "Hello!" },
         { sender: "AmirZand", content: "Hi" },
         { sender: "Amirreza Rezaei", content: "Hey guys!" },
+        { sender: "Amirreza Rezaei", content: "How's the day?" },
+        { sender: "Amirreza Rezaei", content: "How's the day?" },
+        { sender: "Amirreza Rezaei", content: "How's the day?" },
+        { sender: "Amirreza Rezaei", content: "How's the day?" },
+        { sender: "Amirreza Rezaei", content: "How's the day?" },
+        { sender: "Amirreza Rezaei", content: "How's the day?" },
+        { sender: "Amirreza Rezaei", content: "How's the day?" },
+        { sender: "Amirreza Rezaei", content: "How's the day?" },
+        { sender: "Amirreza Rezaei", content: "How's the day?" },
+        { sender: "Amirreza Rezaei", content: "How's the day?" },
+        { sender: "Amirreza Rezaei", content: "How's the day?" },
         { sender: "Amirreza Rezaei", content: "How's the day?" },
     ]
 
